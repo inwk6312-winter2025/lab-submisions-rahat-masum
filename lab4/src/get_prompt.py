f6cb5f5b719b3e00e7456eec0f5bf0e494e8f6c3
Rahat <rahat.masum@dal.com>
2025-02-13 11:21:25 -0400

    Commited for lab-4

---
+++ lab4/src/get_prompt.py
@@ -0,0 +1,33 @@
+from netmiko import Netmiko
+device = [
+        {
+        "device_type": "cisco_ios",
+        "ip": "192.168.1.101", # Router 1
+        "username": "student",
+        "password": "Meilab123",
+        "secret": "cisco",
+        "port": "22",
+        }
+        {
+                    "device_type": "cisco_ios",
+                            "ip": "192.168.1.102", # Router 2
+                                    "username": "student",
+                                            "password": "Meilab123",
+                                                    "secret": "cisco",
+                                                            "port": "22",
+                                                                    }
+        {
+                    "device_type": "cisco_ios",
+                            "ip": "192.168.1.103", # Router 3
+                                    "username": "student",
+                                            "password": "Meilab123",
+                                                    "secret": "cisco",
+                                                            "port": "22",
+                                                                    }
+        ]
+net_connect = Netmiko(**device)
+print(f"Default prompt: {net_connect.find_prompt()}")
+net_connect.send_command_timing("disable")
+print(f"Disable command: {net_connect.find_prompt()}")
+net_connect.enable()
+print(f"Enable command: {net_connect.find_prompt()}")
