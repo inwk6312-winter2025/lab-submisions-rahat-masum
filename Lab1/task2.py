8f8e1f54a9d242f67e442d53b1a2097d89654fc0
rahat-masum <rahat.masum@dal.ca>
2025-02-20 23:34:47 -0400

    lab2

--- Lab1/task2.py
+++
@@ -1,3 +0,0 @@
-import logging
-logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s')
-logging.warning('This is a Warning')
--- Lab1/task3.py
+++
@@ -1,3 +0,0 @@
-import logging
-name = 'John'
-logging.error(f'{name} raised an error')
